4b77fe4cedad9f3c8bc7313ab447d5e052e27065
yassinedahmouch <yassinedahmouch1@gmail.com>
2025-12-27 16:33:08 +0100

    main : init commit

---
+++ src/test/java/com/renault/garage/GarageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renault.garage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
